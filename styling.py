3bf9b389bbda7318297d0c76bc0adb77eff374d6
vetematts <165625991+vetematts@users.noreply.github.com>
2025-12-27 00:27:23 +1000

    Refactor ASCII logo printing to use predefined ASCII art and colorize sections based on coordinates

--- styling.py
+++ styling.py
@@ -1,21 +1,51 @@
 
 
 
-import os
 from colorama import Fore, Style
 
-def print_plex_logo_ascii():
-    file_path = os.path.join(os.path.dirname(__file__), "plex_ascii.txt")
-    with open(file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            # Split the line by "1" to identify the sections
-            parts = line.rstrip("\n").split("1")
+# Custom Plex Yellow Color (Hex: #EBAF01 -> RGB: 235, 175, 1)
+PLEX_YELLOW = "\033[38;2;235;175;1m"
 
-            # If we have 5 parts (P, L, E, Arrow, Rest), color the 4th part (index 3)
-            if len(parts) == 5:
-                print(Fore.WHITE + parts[0] + parts[1] + parts[2] + Fore.YELLOW + parts[3] + Fore.WHITE + parts[4] + Style.RESET_ALL)
-            # If we have 3 parts (Start, Arrow, End), color the middle part
-            elif len(parts) == 3:
-                print(Fore.WHITE + parts[0] + Fore.YELLOW + parts[1] + Fore.WHITE + parts[2] + Style.RESET_ALL)
+PLEX_LOGO_ASCII = r"""
+                      .lOOOl.
+                      .kMMMO.
+   ....   .',,,.      .kMMMO.       .',;,'.    ......      ......
+  .dKKKkokXWWWWX0d,   .kMMMO.   .:oxKNWWWWXOo,.'ldddo,   .oKKKKx'
+  .kMMMMMWX0OKNMMMNd. .kMMMO.  ;0WMMWKkxk0NMMNd'.okkkxc..oWMMWx'
+  .kMMMWk;.  .'oXMMWd..OMMMO. :XMMMXl.   .,OWMWk..:xkkko''xNXl.
+  .kMMMk.       cNMMK,.OMMMO..kMMMMXkxxxxxx0WMMNc  ,dkkkd;.;,
+  .kMMMd        :NMMX;.OMMMO..OMMMMXOOOOOOOOOOOk;  .okkkkc...
+  .kMMMXl.     ;0MMMO..OMMMX: oWMMM0,     .''''.  ,dkkkd;.cKO,
+  .kMMMMWKxoodONMMW0, .xMMMMO'.oNMMMXxlclxKNNXx'.:xkkko..lNMMXl.
+  .kMMMNKXWMMMMMNOl.   'OWMMWd. ,dOKWMMMMMWXkc..lkkkxc.  ,kNMMWx.
+  .kMMMK;.;clll:'.      .,ll;.     .,:lllc;.   .'..'.      ,clc'
+  .kMMM0'
+  .xNXk;
+   ...
+"""
+
+# Coordinates (start_col, end_col) for the yellow chevron on each line
+YELLOW_ZONES = {
+    2: (47, 53),
+    3: (48, 55),
+    4: (49, 56),
+    5: (51, 57),
+    6: (51, 57),
+    7: (51, 58),
+    8: (50, 57),
+    9: (49, 56),
+    10: (48, 55),
+    11: (47, 53),
+}
+
+def print_plex_logo_ascii():
+    lines = PLEX_LOGO_ASCII.strip().splitlines()
+    for i, line in enumerate(lines):
+        if i in YELLOW_ZONES:
+            start, end = YELLOW_ZONES[i]
+            if len(line) >= end:
+                print(Fore.WHITE + line[:start] + PLEX_YELLOW + line[start:end] + Fore.WHITE + line[end:] + Style.RESET_ALL)
             else:
-                print(Fore.WHITE + "".join(parts) + Style.RESET_ALL)
+                print(Fore.WHITE + line + Style.RESET_ALL)
+        else:
+            print(Fore.WHITE + line + Style.RESET_ALL)
