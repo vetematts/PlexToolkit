ab9f7e57e052ffd823c22b44ce509ff967e528e0
vetematts <165625991+vetematts@users.noreply.github.com>
2025-12-30 02:31:24 +1000

    Update welcome message to reflect new name as "Plex Toolkit"

--- main.py
+++ main.py
@@ -320,7 +320,7 @@ def welcome():
     else:  # For macOS and Linux
         os.system("clear")
     print_plex_logo_ascii()
-    print(Fore.CYAN + f"\n{emojis.MOVIE} Welcome to the Plex Collection Creator!")
+    print(Fore.CYAN + f"\n{emojis.MOVIE} Welcome to Plex Toolkit!")
     print(Fore.YELLOW + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n")
 
 
